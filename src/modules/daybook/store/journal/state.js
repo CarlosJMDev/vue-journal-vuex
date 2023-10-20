314b984172fa13be407fb8021a41241c5e578673
Carlos Jimenez <carlosjmdev@gmail.com>
2023-10-20 19:08:09 +0200

    Primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,25 @@
+
+
+export default () => ({
+  isLoading: true,
+  entries: [
+    {
+      id: new Date().getTime(),
+      date: new Date().toDateString(), // Sat 23, july
+      text: 'Et Lorem eu labore id ea eiusmod. Magna laborum officia nisi deserunt cillum do consequat magna consectetur cillum nisi sint. Duis quis ex officia ad et aliqua Lorem enim laborum id pariatur. Aute anim ullamco fugiat eiusmod cillum do. Officia amet laborum duis veniam eu duis. Irure sunt dolore eu irure ea amet qui laboris enim dolor eiusmod ex esse occaecat. Magna in excepteur velit voluptate esse.',
+      picture: null, // https://
+    },
+    {
+      id: new Date().getTime() + 1000,
+      date: new Date().toDateString(), // Sat 23, july
+      text: 'Velit minim cillum anim aliqua sunt magna eiusmod laboris. Eu consequat eiusmod culpa ad. Voluptate in non dolor ullamco ea duis ad magna. Dolore fugiat qui nisi aute dolor elit consequat mollit. Reprehenderit voluptate laborum nostrud ullamco sunt adipisicing nisi nostrud eiusmod do.',
+      picture: null, // https://
+    },
+    {
+      id: new Date().getTime() + 2000,
+      date: new Date().toDateString(), // Sat 23, july
+      text: 'Ipsum ad ipsum magna mollit consequat est laboris irure nostrud occaecat sunt do adipisicing ullamco. Ut exercitation irure exercitation incididunt magna. Do ut consequat laborum minim tempor quis minim elit irure proident nostrud sint ex. Dolore nostrud proident et occaecat laborum quis.',
+      picture: null, // https://
+    }
+  ]
+})
